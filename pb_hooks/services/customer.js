0732878a8e412401eabc1bedf9d299e4faf48a1a
Ferdinand Mütsch <ferdinand@muetsch.io>
2025-07-24 12:29:03 +0200

    feat: customer csv export

--- pb_hooks/services/customer.js
+++ pb_hooks/services/customer.js
@@ -19,6 +19,39 @@ function getUniqueStreets(query, app = $app) {
     return [...new Set(streets)]
 }
 
+function exportCsv(app = $app) {
+    const CSV = require(`${__hooks}/utils/csv.js`)
+
+    const fields = [
+            { id: 'id', label: '_id', empty: '' },
+            { id: 'iid', label: 'ID', empty: '' },
+            { id: 'email', label: 'E-Mail', empty: '' },
+            { id: 'phone', label: 'Telefon', empty: '' },
+            { id: 'firstname', label: 'Vorname', empty: '' },
+            { id: 'lastname', label: 'Nachname', empty: '' },
+            { id: 'street', label: 'Strasse', empty: '' },
+            { id: 'city', label: 'Stadt', empty: '' },
+            { id: 'postal_code', label: 'PLZ', empty: '' },
+            { id: 'heard', label: 'Aufmerksam geworden', empty: '' },
+            { id: 'remark', label: 'Anmerkungen', empty: '' },
+            { id: 'registered_on', label: 'Registriert am', empty: '' },
+            { id: 'renewed_on', label: 'Erneuert am', empty: '' },
+            { id: 'newsletter', label: 'Newsletter', empty: false },
+            { id: 'num_rentals', label: 'Ausleihen', empty: 0 },
+            { id: 'num_active_rentals', label: 'Aktive Ausleihen', empty: 0 },
+        ]
+    
+    const result = arrayOf(new DynamicModel(fields.reduce((acc, field) => {
+        acc[field.id] = field.empty
+        return acc;
+    }, {})))
+    app.db().newQuery('select customer.*, coalesce(num_rentals, 0) as num_rentals, coalesce(num_active_rentals, 0) as num_active_rentals from customer left join customer_rentals using (id)').all(result)
+    const records = JSON.parse(JSON.stringify(result))
+
+    return CSV.serialize({ fields, records })
+}
+
 module.exports = {
     getUniqueStreets,
+    exportCsv,
 }
