683e600a7e9e0a83ff449d2448f8c0b4b5ad7c4f
Ferdinand Mütsch <ferdinand@muetsch.io>
2025-11-22 20:41:17 +0100

    test: customer welcome mail

--- tests/test_customer.js
+++ tests/test_customer.js
@@ -1,36 +1,54 @@
 import * as chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
-import {getAnonymousClient, getClient} from './base.js'
-import {assert} from 'chai'
-import {describe, it, before} from 'mocha'
+import { getAnonymousClient, getClient, initImap, listInbox, getFakeMailAccount, configureSmtp, USERNAME, purgeInbox } from './base.js'
+import { assert } from 'chai'
+import { describe, it, before } from 'mocha'
 
 chai.use(chaiAsPromised)
-
-/*
-E-Mail example:
----------------
-
-import { configureSmtp, getClient, getFakeMailAccount, listInbox, purgeInbox } from "./base.js"
-
-const client = await getClient()
-const mailAccount = await getFakeMailAccount()
-
-await configureSmtp(client, mailAccount.smtp)
-
-const messages = await listInbox(mailAccount.imap)
-console.log(JSON.stringify(messages))
-await purgeInbox(mailAccount.imap)
-*/
 
 describe('Customer', () => {
     let client
     let anonymousClient
+    let imapClient
 
     before(async () => {
         client = await getClient()
         anonymousClient = await getAnonymousClient()
+
+        const mailConfig = await getFakeMailAccount()
+        imapClient = await initImap(mailConfig.imap)
+        await configureSmtp(client, mailConfig.smtp)
+        await purgeInbox(imapClient)
     })
 
-    it('should send a welcome mail to new customers')  // TODO
+    after(async () => {
+        await imapClient.end()
+    })
+
+    afterEach(async () => {
+        await purgeInbox(imapClient)
+    })
+
+    it('should send a welcome mail to new customers', async () => {
+        let testCustomer = await client.collection('customer').create({
+            iid: 2000,
+            firstname: 'Justus',
+            lastname: 'Jonas',
+            email: 'justusjonas@leihlokal-ka.de',
+            phone: '+49123456789012',
+            registered_on: new Date(),
+        })
+
+        assert.isNotNull(testCustomer)
+
+        const messages = await listInbox(imapClient)
+        assert.lengthOf(messages, 1)
+        assert.equal(messages[0].sender, USERNAME)
+        assert.equal(messages[0].subject, 'Herzlich Willkommen im leih.lokal!')
+        assert.deepEqual(messages[0].recipients, [ testCustomer.email ])
+
+        await client.collection('customer').delete(testCustomer.id)
+    })
+
     it('should run customer auto-deletion')  // TODO
 })
