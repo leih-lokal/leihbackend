4b4b2d9b420d803ae1e189b8d5ff7a75f953196b
Ferdinand Mütsch <ferdinand@muetsch.io>
2025-11-25 19:55:45 +0100

    test: implement stats endpoint tests

--- tests/test_misc.js
+++ tests/test_misc.js
@@ -1,8 +1,8 @@
 import * as chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
-import {getAnonymousClient, getClient} from './base.js'
-import {assert} from 'chai'
-import {describe, it, before} from 'mocha'
+import { getAnonymousClient, getClient } from './base.js'
+import { assert } from 'chai'
+import { describe, it, before } from 'mocha'
 
 chai.use(chaiAsPromised)
 
@@ -15,6 +15,25 @@ describe('Misc', () => {
         anonymousClient = await getAnonymousClient()
     })
 
-    it('should handle emergency closing')  // TODO
-    it('should return stats from the stats endpoint')  // TODO
+    it('should handle emergency closing') // TODO
+
+    describe('Stats', () => {
+        it('should return stats from the stats endpoint', async () => {
+            const response = await client.send('/api/stats')
+            assert.deepEqual(response, {
+                active_customers_count: {
+                    '2025-11-01': 2,
+                },
+                new_customers_count: {
+                    '2025-11-01': 2,
+                },
+                rentals_count: {
+                    '2025-11-01': 2,
+                },
+                total_items: {
+                    '2025-11-01': 3,
+                },
+            })
+        })
+    })
 })
